e7f096befc4e3fbbe45a92b497c9ab56662bb7cd
payno <henri.payno@esrf.fr>
2020-10-02 16:52:34 +0200

    if log level not found replace red by lightGray

--- Orange/canvas/application/outputview.py
+++ Orange/canvas/application/outputview.py
@@ -153,7 +153,7 @@ class OutputView(QWidget):
         string = string.replace('[1;34m', '')
         string = string.replace('[1;35m', '')
         
-        color = self.get_log_level(string) or qt.Qt.red
+        color = self.get_log_level(string) or qt.Qt.lightGray
         charformat.setForeground(color)
         self.__text.moveCursor(QTextCursor.End, QTextCursor.MoveAnchor)
         self.__text.setCurrentCharFormat(charformat)
